7887c0d30983f9fe414ba011df7451847f156bde
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2022-05-13 23:01:39 +0800

    Improve logic for existing report

--- frontend/views.py
+++ frontend/views.py
@@ -51,8 +51,6 @@ def get_report(request, group_id=DEFAULT_CONG_GROUP, year=None, month=None):
         raise PermissionError("You are not authorized to view this report")
 
     start_date, end_date = get_date(year=year, month=month)
-    # if not month:
-    #     date_to_display = end_date - relativedelta(months=1)
 
     group_publishers = Publisher.objects.filter(group_id=group_id)
 
@@ -74,23 +72,51 @@ def get_report(request, group_id=DEFAULT_CONG_GROUP, year=None, month=None):
     )
 
 
+def update_existing_report(form, existing_report):
+
+    if not existing_report:
+        return
+
+    if not form:
+        return
+
+    existing_report.hours = form.cleaned_data["hours"]
+    existing_report.placements = form.cleaned_data["placements"]
+    existing_report.bible_studies = form.cleaned_data["bible_studies"]
+    existing_report.return_visits = form.cleaned_data["return_visits"]
+    existing_report.videos = form.cleaned_data["videos"]
+    existing_report.remarks = form.cleaned_data["remarks"]
+    existing_report.save()
+
+
+def create_report(form, creation_date):
+    new_report = form.save()
+    if not creation_date:
+        return
+    new_report.creation_date = creation_date
+    new_report.save()
+
+
 def cong_submission(request, congregation_code=DEFAULT_CONGREGATION):
     if not Congregation.objects.filter(code=congregation_code).exists():
         raise Http404("Congregation does not exist")
 
+    start_date, end_date = get_date()
     if not request.method == "POST":
         return render(
             request,
             "submission.html",
-            {"form": ReportForm(), "congregation_code": congregation_code},
+            {
+                "form": ReportForm(),
+                "congregation_code": congregation_code,
+                "submission_date": f"{calendar.month_name[end_date.month]}, {end_date.year}",
+            },
         )
-    # create a form instance and populate it with data from the request:
     form = ReportForm(request.POST)
 
     if not form.is_valid():
         return render(request, "submission.html", {"form": form})
 
-    start_date, end_date = get_date()
     selected_publisher = form.cleaned_data["publisher"]
     existing_report = Report.objects.filter(
         creation_date__gte=start_date,
@@ -98,36 +124,21 @@ def cong_submission(request, congregation_code=DEFAULT_CONGREGATION):
         publisher=selected_publisher,
     ).first()
 
+    existing_next_month_report = Report.objects.filter(
+        creation_date__gt=end_date,
+        publisher=selected_publisher,
+    ).first()
+
     if "create_next_month" in request.POST:
-        existing_next_month_report = Report.objects.filter(
-            creation_date__gt=end_date,
-            publisher=selected_publisher,
-        ).first()
         if existing_next_month_report:
-            existing_next_month_report.hours = form.cleaned_data["hours"]
-            existing_next_month_report.placements = form.cleaned_data["placements"]
-            existing_next_month_report.bible_studies = form.cleaned_data[
-                "bible_studies"
-            ]
-            existing_next_month_report.return_visits = form.cleaned_data[
-                "return_visits"
-            ]
-            existing_next_month_report.videos = form.cleaned_data["videos"]
-            existing_next_month_report.remarks = form.cleaned_data["remarks"]
-            existing_next_month_report.save()
+            update_existing_report(
+                form=form, existing_report=existing_next_month_report
+            )
         else:
-            new_report = form.save()
-            new_report.creation_date = start_date + relativedelta(months=1)
-            new_report.save()
+            create_report(form=form, creation_date=start_date + relativedelta(months=1))
 
     if "update_existing" in request.POST:
-        existing_report.hours = form.cleaned_data["hours"]
-        existing_report.placements = form.cleaned_data["placements"]
-        existing_report.bible_studies = form.cleaned_data["bible_studies"]
-        existing_report.return_visits = form.cleaned_data["return_visits"]
-        existing_report.videos = form.cleaned_data["videos"]
-        existing_report.remarks = form.cleaned_data["remarks"]
-        existing_report.save()
+        update_existing_report(form=form, existing_report=existing_report)
 
     if "submit_report" in request.POST:
         if existing_report:
@@ -140,6 +151,11 @@ def cong_submission(request, congregation_code=DEFAULT_CONGREGATION):
                     "year": end_date.year,
                 },
             )
-        form.save()
+        if existing_next_month_report:
+            update_existing_report(
+                form=form, existing_report=existing_next_month_report
+            )
+        else:
+            create_report(form=form, creation_date=start_date)
 
     return render(request, "thanks.html", {"name": selected_publisher.name})
